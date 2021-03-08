f8a2a43932521d5743eac1234b64fdc59c9c345b
bryce <jayhspark@gmail.com>
2021-03-08 21:34:41 +0900

    중간에 다시 할려고

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,48 @@
+import React, { useEffect } from "react";
+
 const About = () => {
-  return ( 
-    <div>this is about</div>
+
+  useEffect(() => {
+    func()
+  }, [])
+
+  const func = () => {
+
+    createElement();
+    // addAttribute();		// 아이디가 "para"인 HTML 요소에 속성을 추가함.
+  }
+
+  const createElement = () => {
+    let criteriaNode = document.getElementById("text")
+    let newNode = document.createElement("p")
+    newNode.innerHTML = "새로운 단락입니다.";
+    newNode.setAttribute("id", "para")
+    document.body.insertBefore(newNode, criteriaNode);
+  }
+
+  // const addAttribute = () => {
+  //   document.getElementById("para").setAttribute("style", "color:red")
+  // }
+
+
+
+  return (
+    <>
+      <div>this is about</div>
+      <div className="new-status">
+        <h2>New monolog</h2>
+        <form action="">
+          <textarea /><br />
+          <input type="submit" value="Post" />
+        </form>
+      </div>
+
+      <div className="statuses">
+        <h2>Monologs</h2>
+        <ul></ul>
+      </div>
+    </>
   );
 }
- 
+
 export default About;
